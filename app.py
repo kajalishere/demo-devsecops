38950516c689740670f45ad51fead1a02420c2af
kajalishere <kajal.chadhan@gmail.com>
2026-06-10 15:35:52 -0400

    Update app.py

--- app.py
+++ app.py
@@ -5,5 +5,7 @@ Starts the Flask application
 
 from app.app import app
 
-if __name__ == '__main__':
-    app.run(debug=False, host='127.0.0.1', port=5000)
+import sys
+import os
+sys.path.insert(0, os.path.abspath(os.path.dirname(__file__)))
+from app.app import app
