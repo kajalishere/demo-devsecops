1d4ce15826f41e387169a543539e062a9fdaeade
kajalishere <kajal.chadhan@gmail.com>
2026-06-10 12:45:38 -0400

    feat: add app entry point

--- app.py
+++ app.py
@@ -1,1 +1,9 @@
-print("Hello Devsecops")
+"""
+Demo DevSecOps Application Entry Point
+Starts the Flask application
+"""
+
+from app.app import app
+
+if __name__ == '__main__':
+    app.run(debug=False, host='127.0.0.1', port=5000)
